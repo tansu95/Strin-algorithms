19d54282dfe00f113f09be3b2e2efc8ec039c790
tansu95 <2201bgu@bk.ru>
2021-10-09 16:29:19 +0600

    initial

---
+++ src/StringAlgorithm/Alphabetical.java
@@ -0,0 +1,21 @@
+package StringAlgorithm;
+
+public class Alphabetical {
+    public static void main(String[] args) {
+        System.out.println(isAlphabetical("123abc"));
+        System.out.println(isAlphabetical("aBC"));
+        System.out.println(isAlphabetical("abc"));
+        System.out.println(isAlphabetical("xyzabc"));
+    }
+
+    // Данный алгоритм проверяет находятся ли символы, в ведённой строке, в алфавитном порядке
+    public static boolean isAlphabetical(String s) {
+        s = s.toLowerCase();
+        for (int i = 0; i < s.length() - 1; ++i) {
+            if (!Character.isLetter(s.charAt(i)) || !(s.charAt(i) <= s.charAt(i + 1))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
