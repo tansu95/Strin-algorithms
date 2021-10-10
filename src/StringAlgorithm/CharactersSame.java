d28eadc49bfdb4129d348c0d31c5b7b99d5ce259
tansu95 <2201bgu@bk.ru>
2021-10-10 16:03:35 +0600

    Add class CharactersSame

--- src/StringAlgorithm/CharactersSame.java
+++ src/StringAlgorithm/CharactersSame.java
@@ -1,2 +1,20 @@
-package StringAlgorithm;public class CharactersSame {
+package StringAlgorithm;
+
+public class CharactersSame {
+    public static void main(String[] args) {
+        System.out.println(isAllCharactersSame(""));
+        System.out.println(isAllCharactersSame("aab"));
+        System.out.println(isAllCharactersSame("aaa"));
+        System.out.println(isAllCharactersSame("11111"));
+    }
+
+    // Данный алгоритм проверяет являются ли все символы, в ведённой строке, одинаковыми
+    public static boolean isAllCharactersSame(String s) {
+        for (int i = 1; i < s.length(); ++i) {
+            if (s.charAt(i) != s.charAt(0)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
